95accff532b083efa7df3ad0508e4d9fd60e1440
Sviatilishe <svystoplyaskin@gmail.com>
2023-09-01 23:45:18 +0300

    Add gitignore file

---
+++ js_basics/task5-1.js
@@ -0,0 +1,4 @@
+const radius = 7;
+
+const circleArea = Math.PI * Math.pow(radius, 2);
+console.log("Circle Area:", circleArea.toFixed(2));
---
+++ js_basics/task5-2.js
@@ -0,0 +1,5 @@
+const length = 10;
+const width = 8;
+
+const rectangleArea = length * width;
+console.log("Rectangle Area:", rectangleArea.toFixed(2));
