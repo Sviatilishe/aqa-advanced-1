21d949bce038bcba4c4f14e745626219079d9d4e
Sviatilishe <svystoplyaskin@gmail.com>
2023-09-15 12:56:22 +0300

    Homework 5. Arrays

---
+++ arrays/task6.js
@@ -0,0 +1,8 @@
+const numbersList = [1, 10, 14, 2, 4, 5, 43, 34];
+
+const copiedArray = [...numbersList];
+
+copiedArray.sort((a, b) => a - b);
+
+console.log("Original array: " + numbersList);
+console.log("Sorted array: " + copiedArray);
