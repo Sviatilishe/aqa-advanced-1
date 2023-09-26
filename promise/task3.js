f62df80b8c9e78cfaec41901f46b75f0a132844f
Sviatilishe <svystoplyaskin@gmail.com>
2023-09-26 16:48:08 +0300

    Re-wrote functions using async/await

--- promise/task3.js
+++ promise/task3.js
@@ -1,40 +1,45 @@
-function fetchTodo() {
-    return fetch("https://jsonplaceholder.typicode.com/todos/1").then(
-      (result) => {
-        if (!result.ok) {
-          throw new Error("Internal Server Error (todos)");
-        }
-        return result.json();
-      },
-    );
-  }
-  
-  function fetchUser() {
-    return fetch("https://jsonplaceholder.typicode.com/users/1").then(
-      (result) => {
-        if (!result.ok) {
-          throw new Error("Internal Server Error (users)");
-        }
-        return result.json();
-      },
-    );
-  }
-  
-  async function fetchData() {
-    try {
-      const todo = await fetchTodo();
-      const user = await fetchUser();
-      return { todo, user };
-    } catch (error) {
-      throw error;
+async function fetchTodo() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+
+    if (!response.ok) {
+      throw new Error("Internal Server Error (todos)");
     }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
   }
-  fetchData()
-    .then(({ todo, user }) => {
-      console.log('Result (todo):', todo);
-      console.log('Result (user):', user);
-    })
-    .catch(error => {
-      console.error('Error:', error.message);
-    });
-  
+}
+
+async function fetchUser() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
+
+    if (!response.ok) {
+      throw new Error("Internal Server Error (users)");
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+async function fetchData() {
+  try {
+    const todo = await fetchTodo();
+    const user = await fetchUser();
+
+    console.log("Todo:", todo);
+    console.log("User:", user);
+  } catch (error) {
+    console.error("Failed to fetch data:", error);
+  }
+}
+
+fetchData();
